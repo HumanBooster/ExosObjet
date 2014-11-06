91268cdc42f26cdd8048fbb3b6ae856f5db3135a
HumanBooster <bclerc@humanbooster.com>
2014-11-06 14:32:39 +0100

    hotfix

--- InventaireEric/src/Guitare.java
+++ InventaireEric/src/Guitare.java
@@ -3,7 +3,7 @@ import com.modeliosoft.modelio.javadesigner.annotations.objid;
 @objid ("8dd9ea9d-cafc-4c75-8f56-55e4b542593d")
 public class Guitare extends Instrument {
     @objid ("65558a08-a826-4682-b17c-fb5d29416abf")
-    public Guitare(String numSerie, double prix, PrefGuitare prefGuitare) {
-        super(numSerie, prix, prefGuitare);
+    public Guitare(String numSerie, double prix, PrefGuitare pref) {
+        super(numSerie, prix, pref);
     }
 }
--- InventaireEric/src/Mandoline.java
+++ InventaireEric/src/Mandoline.java
@@ -1,12 +1,10 @@
-import java.util.List;
-
 import com.modeliosoft.modelio.javadesigner.annotations.objid;
 
 @objid ("2f12e654-ff33-48cf-b9f3-b2b6bf1e42ed")
 public class Mandoline extends Instrument {
     @objid ("50a2eb03-efb5-4eed-8529-d91fd26fc315")
-    public Mandoline(String numSerie, double prix, PrefMandoline prefMandoline) {
-        super(numSerie, prix, prefMandoline);
+    public Mandoline(String numSerie, double prix, PrefMandoline pref) {
+        super(numSerie, prix, pref);
     }
    
 }
